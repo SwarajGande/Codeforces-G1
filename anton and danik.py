d96df47346f21ba16431110b105b88c401fe00a7
SAIJEEVAN2002 <56346266+SAIJEEVAN2002@users.noreply.github.com>
2020-03-17 09:59:30 +0530

    Create anton and danik.py

---
+++ anton and danik.py
@@ -0,0 +1,17 @@
+n = int(input())
+s = str(input())
+s1 = list(s)
+d = 0
+a = 0
+for i in range(len(s1)):
+    if s1[i]=='D':
+        d=d+1
+for j in range(len(s1)):
+    if s1[j]=='A':
+        a=a+1
+if a>d:
+    print("Anton")
+elif a==d:
+    print("Friendship")
+else:
+    print("Danik")
