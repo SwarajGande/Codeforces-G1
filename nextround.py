16a4144bdf4f1d14694619f56476d80b62560d91
Swaraj7997 <59888924+Swaraj7997@users.noreply.github.com>
2020-10-06 22:39:58 +0530

    Update nextround.py

--- nextround.py
+++ nextround.py
@@ -1,11 +1,10 @@
 n,k = map(int,input().split())
 a = list(map(int,input().split()))
 c = 0
-for i in range(0,n):
-    if a[k-1]==0 and a[i]==a[k-1]:
-        c=c+0
-    elif a[k-1]<=a[i]:
-        c=c+1
+for i in range(len(a)):
+    if (a[i]>0):
+        if(a[i]>=a[k-1]):
+            c+=1
     else:
         c=c+0
 print(c)
